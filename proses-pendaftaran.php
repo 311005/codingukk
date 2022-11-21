f5cda50044e799d5ce452d4a35e1eef2f658aa24
younger31 <118681549+311005@users.noreply.github.com>
2022-11-21 14:47:14 +0700

    Update proses-pendaftaran.php

--- proses-pendaftaran.php
+++ proses-pendaftaran.php
@@ -6,11 +6,17 @@ include("config.php");
 if(isset($_POST['daftar'])){
 
     $id = $_POST['id'];
-    $nama = $_POST['nama'];
-    $tanggal = $_POST['tanggal_lahir'];
-    $alamat = $_POST['alamat'];
+    $nama_lengkap = $_POST['nama_siswa'];
+    $jurusan_yang_dipilih = $_post[ 'jurusan_yang_dipilih'];
+    $jenis_kelamin = $_post[ 'jenis_kelamin'];
+    $asal_sekolah = $_post[ 'asal_sekolah'];
+    $tanggal_lahir = $_POST['tanggal_lahir'];
+    $agama = $_agama[ 'agama'];
+    $alamat_lengkap = $_POST['alamat_lengkap'];
+    $no_telepon = $_post[ 'no_telepon'];
+    $email = $_post[ 'email'];
 
-    $sql = "INSERT INTO tb_akbar (id, nama, tanggal_lahir, alamat) VALUE ('$id', '$nama', '$tanggal', '$alamat')";
+    $sql = "INSERT INTO tb_pendaftaran_siswa (`id`, `nama_siswa`, `jurusan_yang_dipilih`, `jenis_kelamin`, `asal_sekolah`, `tanggal_lahir`, `agama`, `alamat_lengkap`, `no_telepon`, `email`) VALUES ('$id', '$nama_siswa', '$jurusan_yang_dipilih]', '$jenis_kelamin', '$asal_sekolah', '$tanggal_lahir', '$agama', '$alamat_lengkap', '$no_telepon', '$email')";
     $query = mysqli_query($db, $sql);
 
 
