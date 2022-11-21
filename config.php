ebede2497c2a79485afc3c51fa93f3241392ac2a
younger31 <118681549+311005@users.noreply.github.com>
2022-11-21 14:51:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,9 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$nama_database = "ukk";
-
+$nama_database = "db_mhilalagathan";
 $db = mysqli_connect($host, $user, $password, $nama_database);
-
 if( !$db ){ 
     die("gagal terhubung dengan database" . mysqli_connect_error());
 }
-
 ?>
